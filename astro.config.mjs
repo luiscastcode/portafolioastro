b517c448fc0edaf5924441e471061894dc3a9ea4
Luis Castillo <cncompusistema@gmail.com>
2026-01-25 16:35:32 -0400

    integracion sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://lcastcode.vercel.app/',
   vite: {
     plugins: [tailwindcss()]
   },
