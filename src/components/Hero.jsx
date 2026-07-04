d8c50c93e93b215e8dde8669ef012f0666b9c862
luiscastcode <cncompusistema@gmail.com>
2026-07-04 13:31:23 -0400

    Modificar el Navbar, no funciona en celular

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@
 
 const Hero = () => {
   return (
-    <section id="home" className="relative pt-20 md:pt-28 pb-20 overflow-hidden min-h-[90vh] flex flex-col justify-center">
+    <section id="home" className="relative pt-8 md:pt-20 pb-20 overflow-hidden min-h-[90vh] flex flex-col justify-center">
       <div className="grid-pattern absolute inset-0 z-0"></div>
       <div className="hero-glow"></div>
       
